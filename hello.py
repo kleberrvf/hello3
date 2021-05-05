623382281379121467ca8a8ee54ed1ccd4c01cfc
Kleber <Kleberrvf@gmail.com>
2021-05-05 18:17:36 -0300

    hello7

--- hello.py
+++ hello.py
@@ -1,19 +1,19 @@
 import boto3
-pipeline = boto3.client('codepipeline')
+pipeline = boto3.client('hello')
 
 def lambda_handler(event, context):
 
     # stuff
 
     response = pipeline.put_job_success_result(
-        jobId=event['CodePipeline.job']['id']
+        jobId=event['hello.job']['id']
     )
     return response
 
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Kleber '),
+        body: JSON.stringify('Hello from KleberP '),
     };
     return response;
 };
