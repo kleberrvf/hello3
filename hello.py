e292a1476727954c7a394bd9b6cc5c92cee736f5
Kleber <Kleberrvf@gmail.com>
2021-05-06 11:51:20 -0300

    hello10

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from KleberPs '),
+        body: JSON.stringify('Hello from KleberP '),
     };
     return response;
 };
