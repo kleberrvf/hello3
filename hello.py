c3989cf638369a040120ac08c2eacf8a9587ca44
Kleber <Kleberrvf@gmail.com>
2021-05-06 14:54:03 -0300

    hello17

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
+import boto3
+pipeline = boto3.client('codepipeline')
+
+def lambda_handler(event, context):
+
+    # stuff
+
+    response = pipeline.put_job_success_result(
+        jobId=event['codepipeline.job']['id']
+    )
+    return response
+
 import json
 
 def lambda_handler(event, context):
